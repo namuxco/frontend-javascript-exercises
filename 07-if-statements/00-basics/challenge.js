77bed547d16614c461cf3e49974acd83f6782193
namuxco <namydesign@gmail.com>
2014-07-26 18:31:01 -0600

    Finished If

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,38 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(number, guess){
+  if (number == guess) {
+    return 'You got it!';
+  }
+  else if (number < guess) {
+    return 'Too high';
+  }
+  else if (number > guess) {
+    return 'Too low';
+  }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(first, second, third, fourth){
+  if ((first === 3 || first === 5 || first === 7) && (second === 2) && (third > 4 && third < 101) && (fourth < 9 || fourth > 20)){
+    return "correct";
+  }
+  else {
+    return "incorrect";
+  }
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+  if (item === "MacBook Air" && money >= 999){
+    return true;
+  }
+  else if (item === "MacBook Pro" && money >= 1299){
+    return true;
+  }
+  else if (item === "Mac Pro" && money >= 2499){
+    return true;
+  }
+  else if (item === "Apple Sticker" && money >= 1){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
