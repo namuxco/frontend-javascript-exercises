f80241e706cc291189ea736d89118dcc67f02b10
namuxco <namydesign@gmail.com>
2014-07-31 19:15:31 -0600

    Array done

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,30 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array){
+  array.push(1);
+  array.reverse();
+  return array;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array){
+  var test = array.join('+');
+  return test;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+//how do i add pluses inbetween an array?
+
+
+/*- Create a function named `plusesEverywhere`. This function should:
+  - Take one argument, an array of at least two numbers.
+  - This function should return:
+    - a String made of all the values in the array separated by `+`
+    
+  For example:
+  
+  ```javascript
+  plusesEverywhere([1,2,3]); // returns "1+2+3"
+  plusesEverywhere([18,24]); // returns "18+24"
+  ```
+  */
+
+module.exports.arrayQuantityPlusOne = function(array){
+  return array.length + 1;
+};
