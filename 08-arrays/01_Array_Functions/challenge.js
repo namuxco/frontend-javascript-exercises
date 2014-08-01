e0f030eff5b6c11f6f0c7a4c87da8274835b34d8
namuxco <namydesign@gmail.com>
2014-07-31 19:16:06 -0600

    Resave

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -9,22 +9,6 @@ module.exports.plusesEverywhere = function(array){
   return test;
 };
 
-//how do i add pluses inbetween an array?
-
-
-/*- Create a function named `plusesEverywhere`. This function should:
-  - Take one argument, an array of at least two numbers.
-  - This function should return:
-    - a String made of all the values in the array separated by `+`
-    
-  For example:
-  
-  ```javascript
-  plusesEverywhere([1,2,3]); // returns "1+2+3"
-  plusesEverywhere([18,24]); // returns "18+24"
-  ```
-  */
-
 module.exports.arrayQuantityPlusOne = function(array){
   return array.length + 1;
 };
