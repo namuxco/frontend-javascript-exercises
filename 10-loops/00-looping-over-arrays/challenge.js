5ab49d18b966c3a97dc8176e2f311db0bafa5a1e
namuxco <namydesign@gmail.com>
2014-08-08 14:52:51 -0600

    Help on sumNumbers

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,4 +1,19 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+  for (var i = 0; i < array.length; i++) {
+    var test = array.join('+')
+    }
+  return test;
+};
+
+/* - Complete the `sumNumbers` function. This function should:
+    - return the sum of all numbers inside of the array that is passed to the function
+    - if the array is empty, this function should return 0
+
+    For example:
+    
+    ```javascript
+    sumNumbers([1, 2, 3]) // should return 6
+    */
 
 module.exports.splitAndLowerCaseString = undefined;
 
