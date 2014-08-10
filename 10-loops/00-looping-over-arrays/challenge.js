9d68e2fd9fb14a0db9f3d24d5f7572c241e5c271
The Big Red Geek <rhyneandrew@gmail.com>
2014-08-09 17:24:51 -0700

    fixed, with docs!

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,8 +1,16 @@
 module.exports.sumNumbers = function(array){
-  for (var i = 0; i < array.length; i++) {
-    var test = array.join('+')
-    }
-  return test;
+
+  //so for this function, we want to add up all of the members of the array to create the sum of all the numbers
+  //first, we need a variable to act as the value which we will add the array members to!
+
+  var value = 0, //we are setting this to zero because we won't have a sum until we add on atleast one of the array's members
+      i; //this is our counter!
+
+  for (var i = 0; i < array.length; i++) { //with each member of the array...
+    value += array[i]; //we want to add the member to the current value, compounding in each member to create an overall sum
+  }
+
+  return value; //now we return the value!
 };
 
 /* - Complete the `sumNumbers` function. This function should:
@@ -10,7 +18,7 @@ module.exports.sumNumbers = function(array){
     - if the array is empty, this function should return 0
 
     For example:
-    
+
     ```javascript
     sumNumbers([1, 2, 3]) // should return 6
     */
