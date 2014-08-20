6fe31824abd552ff56d506b176d990c41a018d42
namuxco <namydesign@gmail.com>
2014-08-19 22:57:01 -0600

    extend still not working

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,69 @@
-module.exports.copy = undefined;
+/* - Complete the `copy` function. This function should:
+    - take one object as an argument
+    - create a new object and copy all of the properties of the input into the new object
+    - return the new object
 
-module.exports.extend = undefined;
+    For example:
+    
+    ```javascript
+    copy({name: "Rob"}) // returns a new object {name: "Rob"}
+    ```
+*/
 
-module.exports.hasElems = undefined;
+module.exports.copy = function(obj){
+  var newObj = {},
+        i;
+
+  for(i in obj){
+    if(obj.hasOwnProperty(i)){
+      newObj[i] = obj[i];
+    }
+  }
+  return newObj;
+};
+
+
+/*
+- Complete the `extend` function. This function should:
+    - take two dictionaries as arguments (`dest` and `src`) and copy all of the properties (e.g. key, value pairs) of `src` into `dest`
+    - return a modified `dest`
+
+    For example:
+    
+    ```javascript
+    extend({name: "Rob"}, {age: 101}) // should modify and return `dest` with content of {name: "Rob", age: 101}
+    
+    extend({name: "Rob"}, {name: "Steve"}) // should modify and return `dest` with content of {name: "Steve"}
+    ```
+*/
+
+module.exports.extend = function(dest, src){
+
+  for(var i in src){ 
+    if(!dest.hasOwnProperty(i)){
+      dest[i] = src[i];
+    }
+  }
+  return dest;
+};
+
+
+/*
+- Complete the `hasElems` function. This function should:
+    - take two arguments. The first is an object and the second is an array of elements
+    - return `true` when all of the elements in the array are keys in the object, false otherwise
+*/
+
+module.exports.hasElems = function(object, array){
+  var result = true,
+      i;
+
+  for(i in array){
+    var propertyName = array[i]
+    if(!object.hasOwnProperty([propertyName])){ //propertyName == object[propertyName]){
+      result = false;
+    }
+  }
+  return result;
+};
+
